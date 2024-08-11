461207e1e4ad66959e24fe8e66cb514a1a0ae510
Mahesh <nalawade41@gmail.com>
2024-08-11 10:05:03 +0530

    Updated routes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider, RouteObject } from 'react-router-dom';
 import './App.css';
-import Root from "./routes/root";
-import ErrorPage from './pages/Error';
-import Home from './pages/Home';
-import Generated, { loader as GeneratedLoader } from './pages/Generated';
+import {Routes} from "./routes/routes";
 
-// Define the route objects with TypeScript types
-const routes: RouteObject[] = [
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/generated/:id",
-        element: <Generated />,
-        loader: GeneratedLoader,
-      }
-    ],
-  }
-];
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(Routes);
 
 function App() {
   return (
---
+++ src/routes/routes.tsx
@@ -0,0 +1,25 @@
+import { RouteObject } from 'react-router-dom';
+import Root from "./root";
+import ErrorPage from "../pages/Error";
+import Home from "../pages/Home";
+import Generated, { loader as GeneratedLoader } from "../pages/Generated";
+
+// Define the route objects with TypeScript types
+export const Routes: RouteObject[] = [
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/generated/:id",
+                element: <Generated />,
+                loader: GeneratedLoader,
+            }
+        ],
+    }
+];
